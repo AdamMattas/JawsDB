bd5fed7b8fc5b790764de8ace689cfddc091e8f2
adam fader <adamfader@gmail.com>
2016-08-01 19:03:54 -0400

    added initial files

---
+++ app/config/connection.js
@@ -0,0 +1,42 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+var mysql = require('mysql');
+
+// Edit this connection to make it work with JawsDB.
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "sector001",
+        database: "starwars"
+    },
+    jawsDB: {
+        port: 3306,
+        host: 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'm3jhaiifn50cvyni',
+        password: "x9y8zgizwm2uhmu9",
+        database: "starwars"
+        // TODO: Add your JawsDB connection info here
+    }
+}
+
+
+// TODO: connect your jawsDB connection witht he mysql connection.
+var connection = mysql.createConnection(source.jawsDB);
+
+
+
+// Replace the connection code here with one to connect to you JawsDB connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
